4a9d69d92db775dbe771fbcee067d21f668148fb
karin <kr9277@bs.amalnet.k12.il>
2023-10-27 20:18:31 +0300

    done2

--- app/src/main/java/com/example/proj6/Credits_Activity.java
+++ app/src/main/java/com/example/proj6/Credits_Activity.java
@@ -9,6 +9,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+/** @author Created by karin on 27/10/2023.
+ * @version 0.0
+ * @since 27/10/2023
+ *On this Activity, there is two Text views which are presented, and a context menu which contains 2 options, stay on this screen or return to the main screen
+  */
 
 public class Credits_Activity extends AppCompatActivity implements View.OnCreateContextMenuListener {
     TextView tv1, tv2;
--- app/src/main/java/com/example/proj6/MainActivity.java
+++ app/src/main/java/com/example/proj6/MainActivity.java
@@ -13,6 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+/** @author Created by karin on 27/10/2023.
+ * @version 0.0
+ * @since 27/10/2023
+ *On this Activity, there is Text view which presents the counter, an Edit Text to get the name from the user, 3 buttons - increases the counter, resets the counter and saves the data, and a context menu which contains 2 options, stay on this screen or go to the credits activity.
+ */
 
 public class MainActivity extends AppCompatActivity implements View.OnCreateContextMenuListener {
     EditText et;
@@ -75,14 +80,38 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         }
         return super.onOptionsItemSelected(item);
     }
+    /**
+     * add method
+     * <p> Reacting the first button
+     * </p>
+     *
+     * @param view the view that triggered the method
+     * The method is increasing the counter
+     */
     public void add(View view){
         i++;
         tv.setText(String.valueOf((i)));
     }
+    /**
+     * zero method
+     * <p> Reacting the second button
+     * </p>
+     *
+     * @param view the view that triggered the method
+     * The method is resets the counter
+     */
     public void zero(View view){
         i=0;
         tv.setText(String.valueOf((i)));
     }
+    /**
+     * out_save method
+     * <p> Reacting the third button
+     * </p>
+     *
+     * @param view the view that triggered the method
+     * The method is saving the data and close the application
+     */
     public void out_save(View view){
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("name", name);
@@ -90,6 +119,14 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         editor.commit();
         finish();
     }
+    /**
+     * onActivityForResult method
+     * <p> Reacting the third button
+     * </p>
+     *
+     * the method gets the number of the intent, an int number which will be 1 if it is ok and 0 if not, and the intent.
+     * The method is saving the data and close the application
+     */
     protected void onActivityForResult(int source, int good, @Nullable Intent intent){
         if(intent!=null){
             name = intent.getStringExtra("name");
